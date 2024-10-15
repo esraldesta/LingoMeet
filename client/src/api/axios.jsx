1fc26a7804617ba3ea61af956b317d748c211c2b
Esral Desta <esraldesta@gmail.com>
2024-10-15 07:05:45 +0300

    URL

--- client/src/api/axios.jsx
+++ client/src/api/axios.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const API = axios.create({
   // TODO: replace URL value from env
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://talkmate.onrender.com/api/v1",
 });
 
 
