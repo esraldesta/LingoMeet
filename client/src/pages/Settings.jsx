9b2bde2b6b474e55870803fd37bf321ff8c2f224
Esral Desta <esraldesta@gmail.com>
2024-10-15 07:30:47 +0300

    fix

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,13 +1,9 @@
-import { useTranslation } from "react-i18next";
-
 export default function Settings() {
-    const { t} = useTranslation("global");
-
     return (
     <div>
         
         <p>Settings</p>
-        <p>{t("coming.message")}</p>
+        <p>Comming soon</p>
     </div>
   )
 }
