d8152cbe97a55af8752c2dfd4f8607ae38ba7d43
kalid <kalidadefa@gmail.com>
2025-03-09 13:56:59 -0400

    react query

--- client/src/api/axios.tsx
+++ client/src/api/axios.tsx
@@ -1,31 +1,74 @@
-import axios from "axios";
+// const API = axios.create({
+//   // baseURL: "https://lingomeetbackend.onrender.com/api/v1",
+//   baseURL: "http://localhost:3000/api/v1",
+// });
 
-const API = axios.create({
+// // Response interceptor
+// API.interceptors.response.use(
+//   (response: any) => {
+//     // Handle the response data here
+//     if (response.data && response.data.data.errors) {
+//       return Promise.reject({
+//         response: {
+//           status: 403,
+//           data: response.data,
+//         },
+//       });
+//     }
+//     return response;
+//   },
+//   (error: any) => {
+//     // Handle errors here
+//     if (error.response && error.response.status >= 401) {
+//       // Optionally, redirect the user to the sign-in page
+//       window.location.href = "/notfound";
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default API;
+
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+
+interface NextApiClientInstance extends AxiosInstance {
+  get<T = any, R = T>(url: string, config?: AxiosRequestConfig): Promise<R>;
+}
+
+const API: NextApiClientInstance = axios.create({
   // baseURL: "https://lingomeetbackend.onrender.com/api/v1",
   baseURL: "http://localhost:3000/api/v1",
 });
 
-// Response interceptor
 API.interceptors.response.use(
-  (response: any) => {
-    // Handle the response data here
-    if (response.data && response.data.data.errors) {
-      return Promise.reject({
-        response: {
-          status: 403,
-          data: response.data,
-        },
-      });
-    }
-    return response;
+  (response) => {
+    return response.data;
   },
-  (error: any) => {
-    // Handle errors here
-    if (error.response && error.response.status >= 401) {
-      // Optionally, redirect the user to the sign-in page
-      window.location.href = "/notfound";
+  (error) => {
+    let message: string;
+
+    // Check if the error has a response
+    if (error.response) {
+      // Handle server errors
+      message =
+        error.response.data?.message ||
+        error.message ||
+        error.response.data.error?.message ||
+        "An unknown server error occurred";
+    } else if (error.request) {
+      // Handle request made but no response received
+      message = "No response received from the server";
+    } else {
+      // Handle other types of errors (e.g., setup errors)
+      message = error.message || "An unexpected error occurred.";
     }
-    return Promise.reject(error);
+
+    // Handle network errors specifically
+    if (error.message === "Network Error") {
+      message = "Please check your internet connection.";
+    }
+
+    return Promise.reject({ message });
   }
 );
 
--- client/src/providers/index.tsx
+++ client/src/providers/index.tsx
@@ -1,6 +1,13 @@
+import { queryClient } from "@/api/react-query";
 import { RoomProvider } from "@/context/room-context";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactElement } from "react";
 
 export function Providers({ children }: { children: ReactElement }) {
-  return <RoomProvider>{children}</RoomProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      <RoomProvider>{children}</RoomProvider>;
+    </QueryClientProvider>
+  );
 }
